6c0553bf690565d5dbe13e60037ffbc106fda5bb
Kevin Morissette <morissette.kevin@gmail.com>
2017-04-13 18:54:57 +0000

    Added cookie storage by userID

--- express_server.js
+++ express_server.js
@@ -4,6 +4,10 @@ const app = express();
 const bodyParser = require("body-parser");
 const PORT = 8080;
 
+// Bootstrap require
+// const boot = require("bootstrap");
+// app.use(express.static('public'));
+
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -12,6 +16,20 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 }
+
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+}
+
 //generate alphanumberic string with 6 digits
 function generateRandomString() {
   return Math.random().toString(36).substr(2, 6);
@@ -96,6 +114,21 @@ app.get("/u/:shortURL", (req, res) => {
   }
 })
 
+app.get("/register", (req,res) => {
+  res.render("partials/_register");
+})
+
+app.post("/register", (req,res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  users.user3RandomID = (newUser = {});
+  newUser.id = generateRandomString();
+  newUser.email = email;
+  newUser.password = password;
+  console.log(users);
+  res.cookie("user_id",newUser.id).redirect("/");
+})
+
 app.post("/urls", (req,res) => {
   let randomString = generateRandomString();
   urlDatabase[randomString] = req.body.longURL;
@@ -107,9 +140,9 @@ app.post("/urls/:id/delete", (req,res) => {
   res.redirect("/urls");
 })
 
-// app.use(function (req, res, next) {
-//   res.status(404).send("Sorry, this page does not exist.")
-// })
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry, this page does not exist.")
+})
 
 app.listen(PORT, ()=> {
   console.log(`Tinyapp listening on port ${PORT}!`);
