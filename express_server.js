69891f35e63446be7e4de7a7a8075c87e859af42
Kevin Morissette <morissette.kevin@gmail.com>
2017-04-14 04:28:33 +0000

    Implementation of res.locals and refactoring

--- express_server.js
+++ express_server.js
@@ -21,25 +21,38 @@ app.use(cookieSession({
 
 app.set("view engine", "ejs");
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
-}
-
 const users = {
   "userRandomID": {
     id: "userRandomID",
     email: "user@example.com",
-    password: "purple-monkey-dinosaur"
+    password: "purple-monkey-dinosaur",
+    urls: {"9sm5xK": "http://www.google.com"}
   },
   "user2RandomID": {
     id: "user2RandomID",
     email: "user2@example.com",
-    password: "dishwasher-funk"
+    password: "dishwasher-funk",
+    urls: {"b2xVn2": "http://www.lighthouselabs.ca"}
   }
 }
 
-//generate alphanumberic string with 6 digits
+const auth = (req, res, next) => {
+  if (req.session.user_id) {
+    res.locals.userID = req.session["user_id"];
+    console.log(res.locals.userID);
+    res.locals.userURLS = users[res.locals.userID].urls;
+    next();
+  } else {
+    res.status(401).send("You must log in to access this page.<br><a href='/login'>Login</a>")
+  }
+}
+
+// const urlDatabase = {
+//   "b2xVn2": "http://www.lighthouselabs.ca",
+//   "9sm5xK": "http://www.google.com"
+// }
+
+//generate alphanumberic string with 6 digits for the user id
 function generateRandomString() {
   return Math.random().toString(36).substr(2, 6);
 }
@@ -49,65 +62,53 @@ app.get('/', (req, res) => {
 })
 
 //pass urls_index the URL data
-app.get("/urls", (req,res) => {
-  let templateVars = {
-    urls: urlDatabase,
-    user: req.cookies["user_id"],
-  };
+app.get("/urls", auth, (req, res) => {
+    const userID = req.session["user_id"];
+    // console.log(users[userID]);
+    let templateVars = {
+      urls: users[userID].urls,
+      user: userID
+    }
   res.render("urls_index", templateVars);
 })
 
-app.get("/urls/new", (req,res) => {
-  if (req.loggedIn) {
-    let templateVars = {
-      shortURL: req.params.id,
-      longURL: urlDatabase[req.params.id],
-      user: req.cookies["user_id"],
-    };
-    res.render("urls_new", templateVars);
-  } else {
-    res.redirect("../login");
-  }
+app.get("/urls/new", auth, (req, res) => {
+    res.render("urls_new");
 })
 
-app.get("/urls/:id", (req,res) => {
+app.get("/urls/:id", auth, (req, res) => {
   let templateVars = {
     shortURL: req.params.id,
     longURL: urlDatabase[req.params.id],
-    user: req.cookies["user_id"],
   };
   res.render("urls_show", templateVars)
 })
 
-app.get("/urls", (req,res) => {
+app.get("/urls", auth, (req, res) => {
   let templateVars = {
     urls: urlDatabase,
-    user: req.cookies["user_id"],
+    user: req.session["user_id"],
   };
   res.render("urls_index", templateVars);
 })
 
 
-app.get("/logout", (req,res) => {
-  res.clearCookie("user_id");
+// app.get("/logout", (req,res) => {
+//   req.session = null;
+//   res.redirect("/urls");
+// })
+
+app.post("/logout", (req, res) => {
+  req.session["user_id"] = null;
   res.redirect("/urls");
 })
 
-app.post("/logout", (req,res) => {
-  res.clearCookie("user_id");
-  res.redirect("/urls");
+app.get("/login", (req, res) => {
+  res.render("urls_login", {user: req.session["user_id"]})
 })
 
-app.get("/login", (req,res) => {
-  let templateVars = {
-    urls: urlDatabase,
-    user: req.cookies["user_id"],
-  };
-  res.render("urls_login", templateVars)
-})
-
-app.post("/login", (req,res) => {
-  function authenticated(email,password) {
+app.post("/login", (req, res) => {
+  function authenticated(email, password) {
     for (user in users) {
       if (users[user].email === email && users[user].password === password) {
         return users[user];
@@ -115,17 +116,18 @@ app.post("/login", (req,res) => {
     }
   }
 
-  const authenticatedUser = authenticated(req.body.email,req.body.password)
+  const authenticatedUser = authenticated(req.body.email, req.body.password)
 
   if (authenticatedUser) {
-    res.cookie("user_id",authenticatedUser.id).redirect("/");
+    req.session["user_id"] = (authenticatedUser.id)
+    res.redirect("/");
   } else {
     res.status(403).send("Sorry either that email is not registered or the password is incorrect.<br><a href='/register'>Sign Up</a><br><a href='/login'>Login</a>")
   }
 })
 
 // Update link
-app.post("/urls/:id", (req,res) => {
+app.post("/urls/:id", (req, res) => {
   let newURL = req.body.updatelink;
   let shortURL = req.params.id;
   urlDatabase[shortURL] = newURL;
@@ -141,7 +143,7 @@ app.get("/u/:shortURL", (req, res) => {
   }
 })
 
-app.get("/register", (req,res) => {
+app.get("/register", (req, res) => {
   res.render("urls_register");
 })
 
@@ -157,32 +159,32 @@ function doesUserExist(email) {
   }
 }
 
-app.post("/register", (req,res) => {
+app.post("/register", (req, res) => {
   const { email, password } = req.body;
-  const newUserId = `user${countUsers()+1}RandomId`;
   const id = generateRandomString();
   if (req.body.email === '' || req.body.password === '') {
     res.status(400).send("Please enter an email and password.<br><a href='/register'>Return to Registry</a>")
   } else if (doesUserExist(email)) {
     res.status(400).send("A user by that email is already registered.<br><a href='/register'>Return to Registry</a>")
   } else {
-    users[newUserId] = {
+    users[id] = {
       id,
       email,
-      password
+      password,
+      urls: {}
     };
-    res.cookie("user_id", id)
+    req.session["user_id"] = id;
     res.redirect("/");
   }
 })
 
-app.post("/urls", (req,res) => {
+app.post("/urls", auth, (req, res) => {
   let randomString = generateRandomString();
   urlDatabase[randomString] = req.body.longURL;
   res.redirect("/urls/" + randomString);
 })
 
-app.post("/urls/:id/delete", (req,res) => {
+app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect("/urls");
 })
