c2670bf82ece5d8600fab47af7d69951c8e75217
Kevin Morissette <morissette.kevin@gmail.com>
2017-04-15 22:11:10 +0000

    Added greeting by email

--- express_server.js
+++ express_server.js
@@ -2,27 +2,25 @@
 // request/response methods, sets the port/view egine, and uses the essential middle
 const express = require("express");
 const app = express();
-// Requiring our body-parser which allows us to recieve incoming JSON requests
-// and parses them for use through req.body
+// Requiring our body-parser which allows us to recieve incoming client
+// JSON requests and parses them for use through via the request body (req.body)
 const bodyParser = require("body-parser");
-// Requiring cookie-session which allows for encrypted cookies
+// Requiring cookie-session and bcyrpt which allow for encrypted cookies/password storage
 const cookieSession = require("cookie-session");
 const bcrypt = require("bcrypt")
 const PORT = 8080;
-
-// Bootstrap require
-// const boot = require("bootstrap");
-// app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieSession({
   name: 'session',
   keys: ['user_id'],
 
-  // Cookie Options
+  // Cookie Options (session cookies expire after 24 hours)
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }));
 
+// User authorization middleware that requires the user to be logged in
+// to view any pages in the /urls directory
 app.use("/urls", function(req, res, next) {
   if (req.session.user_id) {
     res.locals.userID = req.session["user_id"];
@@ -32,8 +30,10 @@ app.use("/urls", function(req, res, next) {
   }
 })
 
+// Setting the view engine to ejs so that we can write templates containing HTML and JS together
 app.set("view engine", "ejs");
 
+// User database - note the passwords are stored in hashes
 const users = {
   "userRandomID": {
     id: "userRandomID",
@@ -48,6 +48,7 @@ const users = {
   }
 }
 
+// URL database - note each url is assigned to the user who created it
 const urlDatabase = {
   "b2xVn2": {
     longURL: "http://www.lighthouselabs.ca",
@@ -59,15 +60,21 @@ const urlDatabase = {
   }
 }
 
-//generate alphanumberic string with 6 digits for the user id
+// Generates a 6 character string which is used for the shortURL code and the userID
 function generateRandomString() {
   return Math.random().toString(36).substr(2, 6);
 }
 
+// Renders the register page when the user requests the /register URI
 app.get("/register", (req, res) => {
-  res.render("urls_register");
+  if (req.session.user_id) {
+    res.redirect("/");
+  } else {
+    res.render("urls_register");
+  }
 })
 
+// The user submits their registry info
 app.post("/register", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
@@ -90,7 +97,11 @@ app.post("/register", (req, res) => {
 })
 
 app.get("/login", (req, res) => {
-  res.render("urls_login", {user: req.session["user_id"]})
+  if (req.session.user_id) {
+    res.redirect("/");
+  } else {
+    res.render("urls_login", {user: req.session["user_id"]})
+  }
 })
 
 app.post("/login", (req, res) => {
@@ -99,6 +110,7 @@ app.post("/login", (req, res) => {
       if (users[user].email === email && bcrypt.compareSync(password, users[user].hashed_password, 10)) {
         return users[user];
       }
+      // Break up the if statement into multiple if statements to be able to respond with relevant messages depending on if the email or password does not match
     }
   }
 
@@ -114,14 +126,15 @@ app.post("/login", (req, res) => {
 
 app.post("/logout", (req, res) => {
   req.session["user_id"] = null;
-  res.redirect("/urls");
+  // Give the user a message telling them they have successfully logged out
+  res.redirect("/login");
 })
 
 app.get('/', (req, res) => {
   res.redirect('/urls');
 })
 
-//pass urls_index the URL data
+// pass urls_index the URL database via templateVars
 app.get("/urls", (req, res) => {
   let myLinks = {};
   for (shortURL in urlDatabase) {
@@ -129,8 +142,10 @@ app.get("/urls", (req, res) => {
       myLinks[shortURL] = urlDatabase[shortURL].longURL;
     }
   }
+  // console.log(users[req.session["user_id"]].email);
   let templateVars = {
     urls: myLinks,
+    email: users[req.session["user_id"]]
   }
   res.render("urls_index", templateVars);
 })
@@ -145,16 +160,21 @@ app.post("/urls", (req, res) => {
 })
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  res.render("urls_new", {email: users[req.session["user_id"]]});
 })
 
 app.get("/urls/:id", (req, res) => {
   // const userID = req.session["user_id"];
-  let templateVars = {
-    shortURL: req.params.id,
-    longURL: urlDatabase[req.params.id].longURL
+  if (!urlDatabase[req.params.id]) {
+    res.status(404).send("Sorry that link does not exist.<br><a href='/urls'>Return to Tiny App</a>");
+  } else {
+    let templateVars = {
+      shortURL: req.params.id,
+      longURL: urlDatabase[req.params.id].longURL,
+      email: users[req.session["user_id"]]
+    }
+    res.render("urls_show", templateVars)
   }
-  res.render("urls_show", templateVars)
 })
 
 // Update link
@@ -171,17 +191,21 @@ app.post("/urls/:id/delete", (req, res) => {
 })
 
 app.get("/u/:shortURL", (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL].longURL;
-  if (longURL === undefined) {
+  if (!urlDatabase[req.params.id]) {
     res.status(404).send("Sorry that link does not exist.<br><a href='/urls'>Return to Tiny App</a>");
   } else {
-    res.redirect(longURL);
+    let longURL = urlDatabase[req.params.shortURL].longURL;
+    if (longURL === undefined) {
+      res.status(404).send("Sorry that link does not exist.<br><a href='/urls'>Return to Tiny App</a>");
+    } else {
+      res.redirect(longURL);
+    }
   }
 })
 
-function countUsers() {
-  return Object.keys(users).length;
-}
+// function countUsers() {
+//   return Object.keys(users).length;
+// }
 
 function doesUserExist(email) {
   for (user in users) {
@@ -193,6 +217,7 @@ function doesUserExist(email) {
 
 app.use(function (req, res, next) {
   res.status(404).send("Sorry, this page does not exist.")
+  // Add a response redirect
 })
 
 app.listen(PORT, ()=> {
